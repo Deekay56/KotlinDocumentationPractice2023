18e90d788adca337c3356b3f9cbb3d90b27cd0bd
David Maina <david.kariu6@gmail.com>
2023-04-22 18:02:25 +0300

    Commit for 22.04.23 @17201h

--- src/main/kotlin/Misc Practice.kt
+++ src/main/kotlin/Misc Practice.kt
@@ -1,3 +1,14 @@
 fun main() {
 
 }
+
+fun whenElseList(){
+    val countryList = mutableListOf("Kenya", "Nairobi", "Uganda", "Tanzania")
+    val country: String
+
+    when {
+        "Kenya" in countryList -> println("Nairobi is the capital.")
+        "Uganda" in countryList -> println("Kampala is the capital.")
+    }
+    else -> println("Nope")
+}
