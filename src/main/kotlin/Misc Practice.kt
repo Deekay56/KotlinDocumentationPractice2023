73ef53724511c6c5b244626a2e65f817ade7fc45
David Maina <david.kariu6@gmail.com>
2023-06-20 16:50:39 +0300

    Commit for 20.06.23

--- src/main/kotlin/Misc Practice.kt
+++ src/main/kotlin/Misc Practice.kt
@@ -1,36 +1,23 @@
 fun main() {
 
+    whenElseList()
+}
+
+fun whenElseList() {
+    val countryList = mutableListOf("Kenya", "Somalia", "Uganda", "Tanzania")
 
     println("Enter a country name:")
     val myCountry = readln()
 
-    whenElseList(myCountry)
+    val result = when (countryList.indexOf(myCountry)){
+        0 -> println("The capital is Nairobi.")
+        1-> println("The capital is Mogadishu.")
+        2 -> println("The capital is Kampala.")
+        3-> println("The capital is Dar-es-salaam.")
 
-    val yourName = name()
-    println("Your name is $yourName.")
-
-
-
-}
-
-fun whenElseList(myCountry: String) {
-    val countryList = mutableListOf("Kenya", "Somalia", "Uganda", "Tanzania")
-
-    when {
-        myCountry in countryList->{
-            when (myCountry) {
-            "Kenya" -> println("The capital is Nairobi.")
-            "Uganda"-> println("The capital is Kampala.")
-            else -> println("Nope")
-        }
+        else->println("Cannot find $myCountry in the list!")
     }
-        else->println("Not this one!")
-    }
+    return result
 }
 
-fun name(): String {
-    println("Please enter your name.")
-
-    return readln()
-}
 
