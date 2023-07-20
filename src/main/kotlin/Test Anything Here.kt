e47d19858292a56576c7c7fbfa77f6b57083ad4c
David Maina <david.kariu6@gmail.com>
2023-07-20 18:02:27 +0300

    Merge remote-tracking branch 'GithubNewRepo/master'

--- src/main/kotlin/Test Anything Here.kt
+++ src/main/kotlin/Test Anything Here.kt
@@ -28,6 +28,10 @@ fun main(){
 
     //unlabelled continue statement in a nested for loop
     //this will only skip the iteration for the inner loop
+    //when the value of nestedForLoopInContinue is 2 the continue statement is executed.
+    //which only skips that particular iteration of the inner loop.
+    //the outer loop continues unaffected.
+
     for (nestedForLoopOutContinue in 0..3){
         for (nestedForLoopInContinue in 0..3){
             println("$nestedForLoopOutContinue $nestedForLoopInContinue")
@@ -37,5 +41,47 @@ fun main(){
 
     println()
     println()
+
+    //labelled continue statement in a nested for loop
+    //this will only skip all iterations for both the inner and outer loops
+    //When the value of nestedForLoopInContinue is 2, the labelled continue statement (continue@label) is executed
+    //which causes the program to skip all remaining iterations of both the inner and outer loops labeled with "label."
+
+    label@ for (nestedForLoopOutContinue in 0..3){
+        for (nestedForLoopInContinue in 0..3){
+            println("$nestedForLoopOutContinue $nestedForLoopInContinue")
+            if(nestedForLoopInContinue == 2) continue@label
+        }
+    }
+
+    println()
+    println()
+
+    //unlabelled continue for a simple while-loop
+
+    /*
+
+    The while loop starts. When the condition continueWhileLoop <= 3 is evaluated, it is true because continueWhileLoop is 0, so the loop body is executed.
+    Inside the loop, there is an if condition: if (continueWhileLoop == 1) continue.
+    Since continueWhileLoop is 0 and not equal to 1, the if condition is not satisfied, and the continue statement is not executed.
+    The println(continueWhileLoop) statement is executed, and it prints 0.
+    After the first iteration, continueWhileLoop is incremented to 1, and the second iteration starts.
+    In the second iteration, continueWhileLoop is now 1, which matches the if condition (continueWhileLoop == 1).
+    Therefore, the continue statement is executed, and the loop skips the remaining statements in the loop body, including the println(continueWhileLoop) statement.
+    Since the continue statement causes the loop to skip the second iteration,
+    the loop does not proceed to the third iteration. The condition continueWhileLoop <= 3 is not met due to continueWhileLoop being 1, and the loop terminates.
+    So, the actual output is just 0, and the continue statement in the first iteration causes the loop to skip the second iteration when continueWhileLoop is equal to 1,
+    leading to an early termination of the loop.
+
+     */
+
+    var continueWhileLoop = 0
+
+    while (continueWhileLoop<=3){
+        if (continueWhileLoop == 1) continue
+        println(continueWhileLoop)
+        continueWhileLoop++
+
+    }
 
 }
