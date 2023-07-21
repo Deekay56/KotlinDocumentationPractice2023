8b326c718e3b7a3c0d207f11795826b02724f123
David Maina <david.kariu6@gmail.com>
2023-07-21 18:05:09 +0300

    Merge remote-tracking branch 'GithubNewRepo/master'

--- src/main/kotlin/Test Anything Here.kt
+++ src/main/kotlin/Test Anything Here.kt
@@ -5,6 +5,7 @@ fun main(){
     //This means that when the condition forLoopSimpleContinue == 2 is true, the continue statement is executed.
     // This causes the loop to skip the print statement for that iteration.
     // Therefore, the value 2 is not printed in the output (0, 1, 3)
+
     for(forLoopSimpleContinue in 0..3){
         if (forLoopSimpleContinue == 2) continue
         print(forLoopSimpleContinue)
@@ -57,31 +58,48 @@ fun main(){
     println()
     println()
 
-    //unlabelled continue for a simple while-loop
+    //unlabelled continue for a simple while-loop, continue statement after the print statement
 
-    /*
+    //the "if" statement comes after the println(continueWhileLoop) and continueWhileLoop++ statements.
+    //As a result, the println and increment operations are executed...
+    //...before checking if continueWhileLoop is equal to the variable in question and the continue statement is encountered.
+    //As such, the output below will be 0,1,2,3
+    //In other words, since continueWhileLoopA is never 0 at this point (it is initialized with 0 and then immediately incremented),
+    //the if condition will never be true, and the continue statement will never be executed.
 
-    The while loop starts. When the condition continueWhileLoop <= 3 is evaluated, it is true because continueWhileLoop is 0, so the loop body is executed.
-    Inside the loop, there is an if condition: if (continueWhileLoop == 1) continue.
-    Since continueWhileLoop is 0 and not equal to 1, the if condition is not satisfied, and the continue statement is not executed.
-    The println(continueWhileLoop) statement is executed, and it prints 0.
-    After the first iteration, continueWhileLoop is incremented to 1, and the second iteration starts.
-    In the second iteration, continueWhileLoop is now 1, which matches the if condition (continueWhileLoop == 1).
-    Therefore, the continue statement is executed, and the loop skips the remaining statements in the loop body, including the println(continueWhileLoop) statement.
-    Since the continue statement causes the loop to skip the second iteration,
-    the loop does not proceed to the third iteration. The condition continueWhileLoop <= 3 is not met due to continueWhileLoop being 1, and the loop terminates.
-    So, the actual output is just 0, and the continue statement in the first iteration causes the loop to skip the second iteration when continueWhileLoop is equal to 1,
-    leading to an early termination of the loop.
+    var continueWhileLoopA = 0
 
-     */
-
-    var continueWhileLoop = 0
-
-    while (continueWhileLoop<=3){
-        if (continueWhileLoop == 1) continue
-        println(continueWhileLoop)
-        continueWhileLoop++
-
+    while (continueWhileLoopA<=3){
+        println(continueWhileLoopA)
+        continueWhileLoopA++
+        if (continueWhileLoopA == 0) continue
     }
+
+    println()
+    println()
+
+    ///unlabelled continue for a simple while-loop, continue statement after the print statement
+
+    //continueWhileLoopB is 0, and the loop enters its first iteration.
+    //The loop checks if continueWhileLoopB is equal to 1. Since it is not (it is 0), the if condition is not satisfied, and the print statement is executed, printing 0.
+    //continueWhileLoopB is incremented to 1.
+    //The loop starts its second iteration.
+    //Now continueWhileLoopB is 1, and the loop checks if it is equal to 1. The condition is satisfied, and the continue statement is executed.
+    //The continue statement causes the loop to skip the rest of the current iteration, which means the print statement is skipped.
+    //The loop proceeds to the next iteration, which doesn't happen in this case because the continue statement jumped out of the loop, effectively terminating it.
+
+    var continueWhileLoopB = 0
+
+    while (continueWhileLoopB<=3){
+        if (continueWhileLoopB == 1) continue
+        println(continueWhileLoopB)
+        continueWhileLoopB++
+    }
+
+    println()
+    println()
+
+    println("End of Class!")
+
 
 }
